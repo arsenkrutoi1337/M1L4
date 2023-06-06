4e79583e4229fc261c41043b9070316524d3b0f0
arsenkrutoi1337 <132742507+arsenkrutoi1337@users.noreply.github.com>
2023-06-06 18:01:16 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command(name='bot')
+async def _bot(ctx):
+    """Is the bot cool?"""
+    await ctx.send('Yes, the bot is cool.')
+
+bot.run("MTEwOTM4NTIwOTkxODUyNTQ3MA.GFU48z.oBV62aqKKBYwxUiHmSwot9BeAoHr38lluR0dNI")
